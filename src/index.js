e3194383ee3618c0e6a02e656c360102a362e66d
forLoop94 <charlesuwem94@gmail.com>
2023-05-16 14:09:38 +0100

    Reference HTML elements in index.js file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import './styles.css';
 
+const form = document.querySelector('form');
+const nameInput = document.querySelector('.name-input');
+const scoreInput = document.querySelector('.score-input');
+const display = document.querySelector('.display-section');
