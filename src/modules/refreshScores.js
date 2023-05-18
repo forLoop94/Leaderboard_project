cf9e6004075cebce8656cf268d55f2a11910d662
forLoop94 <charlesuwem94@gmail.com>
2023-05-18 17:41:28 +0100

    Fix linter errors

--- src/modules/refreshScores.js
+++ src/modules/refreshScores.js
@@ -3,7 +3,7 @@ export default async () => {
     const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ig0zUZVxOFVzmvzfuw94/scores');
 
     const data = await response.json();
-    data.result.sort(function(a, b){return b.score - a.score});
+    data.result.sort((a, b) => b.score - a.score);
     return data.result;
   } catch (error) {
     return error;
